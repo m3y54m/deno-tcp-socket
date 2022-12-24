576b324074a7e80df590ad5572487d49e3e3a568
Meysam <meysam.parvizi.ir@gmail.com>
2022-12-24 20:08:15 +0330

    Create `echo_server.ts`

---
+++ echo_server.ts
@@ -0,0 +1,9 @@
+/**
+ * echo_server.ts
+ */
+import { copy } from "https://deno.land/std@0.168.0/streams/conversion.ts";
+const listener = Deno.listen({ port: 8080 });
+console.log("listening on 0.0.0.0:8080");
+for await (const conn of listener) {
+  copy(conn, conn).finally(() => conn.close());
+}
